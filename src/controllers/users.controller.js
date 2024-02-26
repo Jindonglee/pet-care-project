287ce9137d6f2854a4fc354977998f7319ffe190
sinhong jeong <jsh719803@gmail.com>
2024-02-26 23:56:15 +0900

    task : 로그인 컨트롤러 메서드 수정

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,11 +4,11 @@ export class UsersController {
   }
 
   // 회원 가입
-  createUser = async (req, res, next) => {
+  signupUser = async (req, res, next) => {
     try {
       const { email, password, confirmPassword, name } = req.body;
 
-      const user = await this.usersService.createUser(
+      const user = await this.usersService.signupUser(
         email,
         password,
         confirmPassword,
@@ -32,51 +32,9 @@ export class UsersController {
     try {
       const { email, password } = req.body;
 
-      if (!email) {
-        return res.status(400).json({
-          message: "email은 필수 입력값입니다.",
-        });
-      }
-      if (!password) {
-        return res.status(400).json({
-          message: "password는 필수 입력값입니다.",
-        });
-      }
-
-      // 해당하는 이메일이 존재하는지 찾아보기
-      const user = await prisma.users.findFirst({
-        where: {
-          email,
-        },
-      });
-
-      // user가 없으면 가입되지 않은 user이므로 error를 발생시킨다.
-      if (!user) {
-        return res.status(400).json({
-          message: "가입되지 않은 이메일입니다.",
-        });
-      }
-
-      // user가 있으면 password가 맞는지 확인한다.
-      // pawwword가 맞지 않으면 올바르지 않은 비밀번호이므로 error를 발생시킨다.
-      if (user.password !== password) {
-        return res.status(400).json({
-          message: "비밀번호가 올바르지 않습니다.",
-        });
-      }
-
-      // password가 맞으면 로그인이 완료된 것이므로 access token을 돌려준다.
-      const accessToken = jwt.sign(
-        { userId: user.id },
-        process.env.TOKEN_SECRET,
-        {
-          expiresIn: "12h",
-        }
-      );
-      const refreshToken = jwt.sign(
-        { userId: user.userId },
-        process.env.REFRESH_TOKEN_KEY,
-        { expiresIn: "7d" }
+      const { accessToken, refreshToken } = await signinService.signinUser(
+        email,
+        password
       );
 
       // 쿠키에 accessToken 설정
