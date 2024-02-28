887cc6b368a96ead3920108856d3f37850a0fdf6
gimmingeon <mnbqwe123987@naver.com>
2024-02-28 20:48:22 +0900

    예약 추가

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -1,6 +1,87 @@
-export class ReservationController {
-  constructor(reservationService) {
-    this.reservationService = reservationService;
+export class ReserveController {
+    
+  constructor(reserveService) {
+      this.reserveService = reserveService;
   }
-  /**여기에 작성하심 됩니다. */
+
+  // 예약 생성
+  createReserve = async (req, res, next) => {
+
+      const { userId } = req.user;
+      const { sitterId, reservation, petInfo, request } = req.body;
+
+      const reserve = await this.reserveService.createReservation(userId, sitterId, new Date(reservation), petInfo, request);
+
+      if (reserve === "시터가 없습니다") {
+          return res.status(404).json({ message: "존재하지 않는 펫시터입니다" });
+      }
+
+      if (reserve === "이미 예약이 됐습니다") {
+          return res.status(404).json({ message: "이미 예약이 됐습니다" });
+      }
+
+      if (reserve === "다른 날짜를 선택하세요") {
+          return res.status(400).json({ message: "다른 날짜를 선택하세요" });
+      }
+
+      return res.status(201).json({ data: reserve });
+  };
+
+
+
+  getdetailreserve = async (req, res, next) => {
+      const { userId } = req.user
+
+      const reserve = await this.reserveService.getReserveById(userId);
+
+      if (!reserve) {
+          return res.status(404).json({ message: "예약 조회에 실패했습니다." });
+      }
+
+      return res.status(200).json({ data: reserve });
+  };
+
+ // 예약 수정 부분
+  updateReserve = async (req, res, next) => {
+      const { userId } = req.user;
+      const { sitterId, reservation, petInfo, request } = req.body;
+
+      const reserve = await this.reserveService.updateReserve(userId, sitterId, new Date(reservation), petInfo, request);
+
+      if (reserve === "다른 날짜를 선택하세요") {
+          return res.status(400).json({ message: "다른 날짜를 선택하세요" });
+      }
+
+      if (reserve === "시터가 없습니다") {
+          return res.status(400).json({ message: "해당 시터가 없습니다" });
+      }
+
+      if (reserve === "예약이 되어있지 않습니다") {
+          return res.status(400).json({ message: "예약이 되어있지 않습니다" });
+      }
+
+      if (reserve === "예약 수정은 확인 대기 중일 때만 가능합니다") {
+          return res.status(400).json({ message: "예약 수정은 확인 대기 중일 때만 가능합니다" });
+      }
+      
+
+      return res.status(200).json({ data: reserve });
+  };
+
+  deleteReserve = async (req, res, next) => {
+      const { userId } = req.user;
+
+      const reserve = await this.reserveService.deleteReserve(userId);
+
+      if (reserve === "삭제할 예약이 없습니다") {
+          return res.status(404).json({ message: "삭제할 예약이 없습니다" });
+      }
+
+      if (reserve === "예약 수정은 확인 대기 중일 때만 가능합니다") {
+          return res.status(400).json({ message: "예약 수정은 확인 대기 중일 때만 가능합니다" });
+      }
+
+      return res.status(200).json({ message: "예약이 삭제되었습니다" });
+  }
+
 }
--- src/repositories/reservation.repository.js
+++ src/repositories/reservation.repository.js
@@ -1,6 +1,123 @@
-export class ReservationRepository {
-  constructor(prisma) {
-    this.prisma = prisma;
+export class ReserveRepository {
+
+  constructor (prisma) {
+      this.prisma = prisma
   }
-  /**여기에 작성하심 됩니다. */
+
+  createReservation = async (userId,sitterId, reservation, petInfo, request) => {
+
+      const createreserve = await this.prisma.reservation.create({
+          data: {
+              userId: +userId,
+              sitterId: +sitterId,
+              reservation,
+              petInfo,
+              request,
+          },
+      });
+      return createreserve;
+  };
+
+  // 날짜 체크, 상태 체크
+  checkDate = async (userId, sitterId, reservation) => {
+      const checkdate = await this.prisma.reservation.findFirst({
+          where : {
+              userId: +userId,
+              sitterId: +sitterId,
+              reservation: { 
+                  equals: new Date(reservation) 
+              }
+          }
+          
+      });
+
+      return checkdate !== null;
+  }
+
+  ExitReservation = async (userId) => {
+      const reservationcheck = await this.prisma.reservation.findFirst({
+          where: {userId: +userId}, 
+      });
+
+      return reservationcheck;
+  }
+
+  findSitter = async (sitterId) => {
+      // 펫시터가 있는지 확인
+      const petsittercheck = await this.prisma.petsitters.findFirst({
+          where: {sitterId: +sitterId}
+      });
+
+      return petsittercheck;
+  };
+
+  getReserveById = async (userId) => {
+      const reserve = await this.prisma.reservation.findFirst({
+          where: { userId: +userId },
+          select: {
+              postId: true,
+              userId : true,
+              sitterId: true,
+              reservation: true,
+              petInfo: true,
+              request: true,
+              reserveState: true,
+              createdAt: true,
+              updateAt: true 
+          },
+      });
+      return reserve;
+  };
+
+  findReservation = async (userId) => {
+
+      const reservationcheck = await this.prisma.reservation.findFirst({
+          where: {userId: +userId}, 
+          select: {postId: true}
+      });
+
+      return reservationcheck;
+  };
+
+  checkState = async (userId) => {
+      const checkstate = await this.prisma.reservation.findFirst({
+          where: {userId: +userId},
+          select: {
+              reserveState: true
+          }
+      });
+
+      return checkstate;
+  }
+
+  updateReserve = async (postId, sitterId, reservation, petInfo, request, reserveStatus) => {
+
+      const reserve = await this.prisma.reservation.update({
+          where: {
+              postId: +postId,
+          },
+          
+          data: {
+              sitterId,
+              reservation, 
+              petInfo, 
+              request,
+              reserveStatus
+          },
+          
+      });
+
+      if(!reserve) {
+          return null;
+      }
+
+      return reserve;
+  };
+
+  deleteReserve = async (postId) => {
+      await this.prisma.reservation.delete({
+          where: { postId: postId },
+      });
+
+  };
 }
