37e83abb712df108b53e02e73afa368a5847f3ae
sinhong jeong <jsh719803@gmail.com>
2024-02-28 22:55:07 +0900

    refactor : authRepository class 생성

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -2,4 +2,8 @@ export class authRepository {
   constructor(prisma) {
     this.prisma = prisma;
   }
+
+  findUserById = async (userId) => {
+    return await prisma.users.findFirst({ where: { userId } });
+  };
 }
