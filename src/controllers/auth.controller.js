90602e60e21575be0d58479621b888ddb92b8bcd
sinhong jeong <jsh719803@gmail.com>
2024-02-28 22:11:47 +0900

    task : auth 컨트롤러,서비스,레포지토리 파일 생성

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+export class authController {
+  constructor(authService) {
+    this.authService = authService;
+  }
+}
---
+++ src/services/auth.service.js
@@ -0,0 +1,5 @@
+export class authController {
+  constructor(authRepository) {
+    this.authRepository = authRepository;
+  }
+}
